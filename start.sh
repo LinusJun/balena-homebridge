0ece47691863ad268c7a54b05cba5a8401d45e71
LinusJun <SonyIT@gmail.com>
2019-06-08 14:26:57 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Start resin-wifi-connect
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-./resin-wifi-connect --clear=false
+ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+#./resin-wifi-connect --clear=false
 
 # At this point the WiFi connection has been configured and the device has
 # internet - unless the configured WiFi connection is no longer available.
